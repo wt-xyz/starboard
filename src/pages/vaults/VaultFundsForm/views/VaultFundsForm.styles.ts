5fc2a524a70497351600720a91cf0759358306ef
Kacper Żmijak <kzmijak@gmail.com>
2025-11-10 15:50:03 +0100

    feat(ui,web): display switchable deposit and withdraw forms

--- src/pages/vaults/VaultFundsForm/views/VaultFundsForm.styles.ts
+++ src/pages/vaults/VaultFundsForm/views/VaultFundsForm.styles.ts
@@ -4,4 +4,8 @@ export default {
   RootStyle: styled.div`
     padding: 1.5px;
   `,
+
+  FormBox: styled.div<{ active: boolean }>`
+    display: ${(props) => (props.active ? 'block' : 'none')};
+  `,
 };
--- src/pages/vaults/VaultFundsForm/views/VaultFundsForm.tsx
+++ src/pages/vaults/VaultFundsForm/views/VaultFundsForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
+import { DepositFundsForm } from '../components/DepositFundsForm';
 import { OperationType, OperationTypeSwitch } from '../components/OperationTypeSwitch';
+import { WithdrawFundsForm } from '../components/WithdrawFundsForm';
 import $ from './VaultFundsForm.styles';
 
 export const VaultFundsForms = () => {
@@ -9,6 +11,13 @@ export const VaultFundsForms = () => {
   return (
     <$.RootStyle>
       <OperationTypeSwitch operationType={operation} onOperationTypeChange={setOperation} />
+
+      <$.FormBox active={operation === OperationType.DEPOSIT}>
+        <DepositFundsForm />
+      </$.FormBox>
+      <$.FormBox active={operation === OperationType.WITHDRAW}>
+        <WithdrawFundsForm />
+      </$.FormBox>
     </$.RootStyle>
   );
 };
