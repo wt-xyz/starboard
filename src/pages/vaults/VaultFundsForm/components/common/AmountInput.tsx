aa571a0d624b5bd186e7d042d88c2ade8d287c5f
Kacper Żmijak <kzmijak@gmail.com>
2025-11-10 15:43:24 +0100

    chore(web): introduce components to be used within vault forms modules

---
+++ src/pages/vaults/VaultFundsForm/components/common/AmountInput.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react';
+
+import { useController } from 'react-hook-form';
+import { NumberFormatValues } from 'react-number-format';
+
+import { ButtonSize } from '@/constants/buttons';
+
+import { useRequiredContext } from '@/hooks/useRequiredContext';
+
+import { DetailsItem } from '@/components/Details';
+import { FormInput } from '@/components/FormInput';
+import { FormMaxInputToggleButton } from '@/components/FormMaxInputToggleButton';
+import { InputType } from '@/components/Input';
+import { WithDetailsReceipt } from '@/components/WithDetailsReceipt';
+
+import { VaultFormContext } from '../../contexts/VaultForm.context';
+
+type AmountInputProps = {
+  label: string;
+  disabled: boolean;
+  maxAmount: string;
+  receiptItems: DetailsItem[];
+};
+
+export const AmountInput: FC<AmountInputProps> = ({ label, disabled, maxAmount, receiptItems }) => {
+  const { control } = useRequiredContext(VaultFormContext);
+  const { field } = useController({ control, name: 'amountInUsdString' });
+
+  const handleAmountChange = (change: NumberFormatValues) => {
+    field.onChange(change.value);
+  };
+
+  const handleMaxClick = (isActive: boolean) => {
+    if (!isActive) field.onChange(maxAmount);
+    else field.onChange('');
+  };
+
+  const MaxToggleButton = (
+    <FormMaxInputToggleButton
+      size={ButtonSize.XSmall}
+      isInputEmpty={field.value === ''}
+      isLoading={false}
+      disabled={disabled}
+      onPressedChange={handleMaxClick}
+    />
+  );
+
+  return (
+    <WithDetailsReceipt side="bottom" detailItems={receiptItems}>
+      <FormInput
+        type={InputType.Currency}
+        label={label}
+        value={field.value}
+        onChange={handleAmountChange}
+        disabled={disabled}
+        slotRight={MaxToggleButton}
+      />
+    </WithDetailsReceipt>
+  );
+};
