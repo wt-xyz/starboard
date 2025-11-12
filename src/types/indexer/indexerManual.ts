4f1074d6c7a2deb90bfa8b0791f0b9913ad9c95f
Drinor Dalipi <drinordalipi5@gmail.com>
2025-11-12 11:34:04 +0100

    chore(indexer): update TypeScript configuration and types
    - Update indexer tsconfig.json for new provider files
    - Update indexerManual.ts with additional type exports

--- src/types/indexer/indexerManual.ts
+++ src/types/indexer/indexerManual.ts
@@ -1,5 +1,3 @@
-import { PartialBy } from '@/lib/typeUtils';
-
 import {
   IndexerAPIOrderStatus,
   IndexerAPITimeInForce,
@@ -22,6 +20,9 @@ import {
   IndexerTradeResponseObject,
   IndexerTransferResponseObject,
 } from './indexerApiGen';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 export interface IndexerCompositeFillResponse {
   pageSize?: number;
