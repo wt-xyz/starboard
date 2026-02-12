3e72fb0f4473ab0a1e64fff1bcadbaf64c2f6248
lukasz-glen <lukasz.glen@poczta.fm>
2026-02-12 14:27:31 +0100

    fix(indexer): fixed websockets

--- indexer/e2e/populate-events-liquidation.ts
+++ indexer/e2e/populate-events-liquidation.ts
@@ -14,7 +14,7 @@ import {
   moveBlockchainTime,
   toPrice,
   walletToAddressIdentity,
-} from './utils';
+} from './utils.js';
 
 // graphql url is hardcoded, taken form the fuel node starting script
 const graphQLUrl = 'http://127.0.0.1:4000/v1/graphql';
