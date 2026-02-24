95a97bad8375db1bcac736a20ece6b318752683a
lukasz-glen <lukasz.glen@poczta.fm>
2026-02-24 12:03:10 +0100

    fix(indexer): trade volume 24h view improved

--- indexer/db/migrations/1768329135951-Data.js
+++ indexer/db/migrations/1768329135951-Data.js
@@ -7,7 +7,7 @@ export default class Data1768329135951 {
         `CREATE VIEW "open_interest" AS SELECT position_key.index_asset_id AS index_asset_id, sum(CASE WHEN position_key.is_long THEN position.size ELSE 0 END) AS open_interest_long, sum(CASE WHEN position_key.is_long THEN 0 ELSE position.size END) AS open_interest_short FROM position LEFT JOIN position_key ON position.position_key_id=position_key.id GROUP BY position_key.index_asset_id`
       );
       await db.query(
-        `CREATE VIEW "trade_volume_24h" AS SELECT position_key.index_asset_id AS index_asset_id, sum(position.size_delta) AS trade_volume FROM position LEFT JOIN position_key ON position.position_key_id=position_key.id WHERE position.timestamp>=extract(epoch from now())-86400 GROUP BY position_key.index_asset_id`
+        `CREATE VIEW "trade_volume_24h" AS SELECT t1.index_asset_id, t1.trade_volume-COALESCE(t2.trade_volume, 0) AS trade_volume_24h FROM (SELECT index_asset_id, trade_volume FROM trade_volume WHERE (index_asset_id, timestamp) IN (SELECT index_asset_id, MAX(timestamp) FROM trade_volume GROUP BY index_asset_id)) t1 LEFT JOIN (SELECT index_asset_id, trade_volume FROM trade_volume WHERE (index_asset_id, timestamp) IN (SELECT index_asset_id, MAX(timestamp) FROM trade_volume WHERE timestamp < extract(epoch from now())-86400 GROUP BY index_asset_id)) t2 ON t1.index_asset_id = t2.index_asset_id`
       );
     }
   
